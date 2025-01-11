71171700c91b4a9c43351d3c62f14ec1e82e3e8f
dereko1 <1021920981@qq.com>
2025-01-11 22:52:38 +0800

    Dereko:添加控制模块

---
+++ Move-dev/Core/BSP/Control/control.c
@@ -0,0 +1,6 @@
+// control.c
+// Author: Dereko
+// Date: 2025-01-11
+#include "control.h"
+
+// Your C functions and implementation go here
---
+++ Move-dev/Core/BSP/Control/control.h
@@ -0,0 +1,10 @@
+// control.h
+// Author: Dereko
+// Date: 2025-01-11
+
+#ifndef CONTROL_H
+#define CONTROL_H
+
+// Your function prototypes and declarations go here
+
+#endif // CONTROL_H
