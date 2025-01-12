648bcab68f1ad996982fed00e08bb341fdb57353
dereko1 <1021920981@qq.com>
2025-01-12 09:49:40 +0800

    Dereko：提交宏定义

--- Move-dev/Core/BSP/BSP_conf.h
+++ Move-dev/Core/BSP/BSP_conf.h
@@ -7,43 +7,38 @@
 
 #include "main.h"
 
-//右上轮接口信息
-/******************RU轮********************/
-#define RU_EN_TYPE GPIOA
-#define RU_EN_PIN GPIO_PIN_5
-#define RU_STP_TYPE RU_STP_GPIO_Port
-#define RU_STP_PIN RU_STP_Pin
-#define RU_DIR_TYPE RU_DIR_GPIO_Port
-#define RU_DIR_PIN RU_DIR_Pin
-/******************LU轮********************/
-#define LU_EN_TYPE GPIOA
-#define LU_EN_PIN GPIO_PIN_5
-#define LU_STP_TYPE GPIOA
-#define LU_STP_PIN GPIO_PIN_1
-#define LU_DIR_TYPE GPIOA
-#define LU_DIR_PIN GPIO_PIN_7
-/******************RL轮********************/
-#define RL_EN_TYPE GPIOA
-#define RL_EN_PIN GPIO_PIN_5
-#define RL_STP_TYPE GPIOA
-#define RL_STP_PIN GPIO_PIN_1
-#define RL_DIR_TYPE GPIOA
-#define RL_DIR_PIN GPIO_PIN_7
-/******************LL轮********************/
-#define LL_EN_TYPE GPIOA
-#define LL_EN_PIN GPIO_PIN_5
-#define LL_STP_TYPE GPIOA
-#define LL_STP_PIN GPIO_PIN_1
-#define LL_DIR_TYPE GPIOA
-#define LL_DIR_PIN GPIO_PIN_7
+//???????????
+#define RU_EN_TYPE GPIOF
+#define RU_EN_PIN GPIO_PIN_0
+#define RU_STP_TYPE GPIOF
+#define RU_STP_PIN GPIO_PIN_1
+#define RU_DIR_TYPE GPIOF
+#define RU_DIR_PIN GPIO_PIN_2
+#define LU_STP_TYPE GPIOF
+#define LU_STP_PIN GPIO_PIN_3
+#define LU_DIR_TYPE GPIOF
+#define LU_DIR_PIN GPIO_PIN_4
+#define RL_STP_TYPE GPIOF
+#define RL_STP_PIN GPIO_PIN_5
+#define RL_DIR_TYPE GPIOF
+#define RL_DIR_PIN GPIO_PIN_6
+#define LL_STP_TYPE GPIOF
+#define LL_STP_PIN GPIO_PIN_7
+#define LL_DIR_TYPE GPIOF
+#define LL_DIR_PIN GPIO_PIN_8
+
+
 //...其他轮定义方式以此类推（RU、LU、RL、LL）
+#define BASESPEED 3200
+#define SPEEDFACTOR 3.9999
+#define STPES_PER_REVOLUTION 3200
+#define STPE_ANGLE (360.0/STPES_PER_REVOLUTION)
+#define SPEED (BASESPEED*SPEEDFACTOR)
 
-
-#define STPES_PER_REVOLUTION 3200 //转一圈脉冲次数
-#define STPE_ANGLE (360.0/STPES_PER_REVOLUTION) //脉冲数换算角度
 typedef struct{
     GPIO_TypeDef* def;
     uint16_t pin;
 }gpio_Conf;
+
 
 #endif //INC_42_DEV_BSP_CONF_H
