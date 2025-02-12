0a5f02a8f805b79b3a55058df1948a9a5e7cbd1d
Franki <caihongfrank@yeah.net>
2025-02-12 15:52:58 +0800

    Franki:视觉初步尝试

--- Visual-control/get-version.py
+++ Visual-control/get-version.py
@@ -15,7 +15,6 @@ class Version:
             if not ret:
                 print("无法读取摄像头数据")
                 break
-
             # 将帧转换为灰度图像
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
