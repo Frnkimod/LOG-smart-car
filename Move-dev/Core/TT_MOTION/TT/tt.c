1a74c2220004f81aa36cc2f0ad0ef75763204d94
dereko1 <1021920981@qq.com>
2025-02-19 12:41:35 +0800

    Dereko:tt电机运动测试

---
+++ Move-dev/Core/TT_MOTION/TT/tt.c
@@ -0,0 +1,18 @@
+// tt.c
+// Author: Dereko
+// Date: 2025-02-18
+#include "tt.h"
+#include "tt.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// 电机控制函数：根据输入参数控制电机的运动
+// 主函数：程序入口
+
+// 计算运动所需的圈数
+
+int TT_main() {
+    TT_motion(&TT,3000,1,180);
+    return 0;       // 程序正常结束
+}
+// Your C functions and implementation go here
---
+++ Move-dev/Core/TT_MOTION/TT/tt.h
@@ -0,0 +1,14 @@
+// tt.h
+// Author: Dereko
+// Date: 2025-02-18
+#include "../../move/move/move.h"
+#ifndef TT_H
+#define TT_H
+#include "../../Core/move/move/move.h"
+#include "../../BSP/42-dev/Emm_V5.h"
+#include "../../Core/LG/check/check_line.h"
+// Your function prototypes and declarations go here
+int TT_main();
+void testTT_motion();
+void TT_CHECK_motion(int32_t speed, int16_t dir, int32_t angle);
+#endif // TT_H
