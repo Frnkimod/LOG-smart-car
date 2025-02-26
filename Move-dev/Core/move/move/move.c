0455a5aea4c0c2a22d029a85a655af2f09c1f240
dereko1 <1021920981@qq.com>
2025-02-26 21:51:28 +0800

    修改

--- Move-dev/Core/move/move/move.c
+++ Move-dev/Core/move/move/move.c
@@ -151,22 +151,24 @@ void linearMovement(Motor *motor1, Motor *motor2, Motor *motor3, Motor *motor4, 
 void tt_right(){
     TT_motion(&TT,4000,0,180);//放置左边孔位
     HAL_Delay(1000);
-    while (!HAL_GPIO_ReadPin(FLAG_IN_GPIO_Port, FLAG_IN_Pin));
-    //等待
-    TT_motion(&TT,4000,1,180);//回到中心
     HAL_GPIO_WritePin(FLAG_OUT_GPIO_Port,FLAG_OUT_Pin,GPIO_PIN_SET);
     HAL_Delay(100);
     HAL_GPIO_WritePin(FLAG_OUT_GPIO_Port,FLAG_OUT_Pin,GPIO_PIN_RESET);
+    while (!HAL_GPIO_ReadPin(FLAG_IN_GPIO_Port, FLAG_IN_Pin));
+    //等待
+    TT_motion(&TT,4000,1,180);//回到中心
+
 }
 void tt_left(){
     TT_motion(&TT,4000,1,180);//放置右边孔位
     HAL_Delay(1000);
-    while (!HAL_GPIO_ReadPin(FLAG_IN_GPIO_Port, FLAG_IN_Pin));
-    //等待
-    TT_motion(&TT,4000,0,180);//回到中心
     HAL_GPIO_WritePin(FLAG_OUT_GPIO_Port,FLAG_OUT_Pin,GPIO_PIN_SET);
     HAL_Delay(100);
     HAL_GPIO_WritePin(FLAG_OUT_GPIO_Port,FLAG_OUT_Pin,GPIO_PIN_RESET);
+    while (!HAL_GPIO_ReadPin(FLAG_IN_GPIO_Port, FLAG_IN_Pin));
+    //等待
+    TT_motion(&TT,4000,0,180);//回到中心
+
 }
 // 主程序
 int move_main() {
